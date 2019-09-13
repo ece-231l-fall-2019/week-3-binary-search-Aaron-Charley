c78bb258755d91efc3e648f4d40b59c662b6b81a
Aaron-Charley <aacharley@unm.edu>
2019-09-13 14:42:32 -0600

    Update runtests.cpp

--- runtests.cpp
+++ runtests.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 #include "Timer.h"
 #include "search.h"
+
+void readFile(std::vector<int>& this_vect, std::string name)
+{
+	std::ifstream fin(name);
+	while(true)
+	{
+		int num;
+		fin >> num;
+		if (fin.eof())
+			break;
+		this_vect.push_back(num);
+	}
+
+}
 
 int main()
 {
 	std::vector<int> numbers;
 	std::vector<int> search;
 
-	// TODO:
-	// read the file "numbers" into the numbers vector
-	// read the file "search" into the search vector
-	// since you're basically doing the same thing twice,
-	// write a function to read a single file into a vector
-	// and then call this function twice.
+	readFile(numbers, "numbers");
+	readFile(search, "search");
+
 
 	{
-		Timer timer("Time to linear search all values: ");
+		Timer timer("Time to search all linear values: ");
 
 		int found = 0;
 		for (size_t i = 0; i < search.size(); i++)
@@ -32,7 +44,7 @@ int main()
 	}
 
 	{
-		Timer timer("Time to linear search all values (pointers): ");
+		Timer timer("Time to all linear with pointers: ");
 
 		int found = 0;
 		for (size_t i = 0; i < search.size(); i++)
@@ -46,9 +58,33 @@ int main()
 			<< search.size() << " values." << std::endl;
 	}
 
-	// TODO:
-	// repeat the above two blocks but use the binary search functions.
+	
+        {
+                Timer timer("Time to search all binary values: ");
 
-	return 0;
-}
+                int found = 0;
+                for (size_t i = 0; i < search.size(); i++)
+                {
+                        if (binarySearch(numbers, search[i]))
+                                found++;
+                }
+
+                std::cout << "Found "<< found << "/"
+                        << search.size() << " values." << std::endl;
+        }
+
+        {
+                Timer timer("Time to search all binary values with pointer: ");
+
+                int found = 0;
+                for (size_t i = 0; i < search.size(); i++)
+                {
+                        if (binarySearch(numbers.data(), numbers.data() + numbers.size(),
+                                        search[i]))
+                                found++;
+                }
+
+                std::cout << "Found "<< found << "/"
+                        << search.size() << " values." << std::endl;
+
 
