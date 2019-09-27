d9df70e95d59faaf36ac2c1b7a93342eceef53a0
Aaron-Charley <aacharley@unm.edu>
2019-09-27 14:52:18 -0600

    Update runtests.cpp

--- runtests.cpp
+++ runtests.cpp
@@ -6,6 +6,7 @@
 #include "Timer.h"
 #include "search.h"
 
+
 void readFile(std::vector<int>& this_vect, std::string name)
 {
 	std::ifstream fin(name);
@@ -13,24 +14,24 @@ void readFile(std::vector<int>& this_vect, std::string name)
 	{
 		int num;
 		fin >> num;
-		if (fin.eof())
+		if(fin.eof())
 			break;
 		this_vect.push_back(num);
 	}
-
 }
-
 int main()
 {
 	std::vector<int> numbers;
 	std::vector<int> search;
 
+	numbers.reserve(1000000);
+	search.reserve(2000);
+
 	readFile(numbers, "numbers");
 	readFile(search, "search");
-
-
+		
 	{
-		Timer timer("Time to search all linear values: ");
+		Timer timer("Time to linear search all values: ");
 
 		int found = 0;
 		for (size_t i = 0; i < search.size(); i++)
@@ -44,7 +45,7 @@ int main()
 	}
 
 	{
-		Timer timer("Time to all linear with pointers: ");
+		Timer timer("Time to linear search all values (pointers): ");
 
 		int found = 0;
 		for (size_t i = 0; i < search.size(); i++)
@@ -57,38 +58,58 @@ int main()
 		std::cout << "Found "<< found << "/"
 			<< search.size() << " values." << std::endl;
 	}
-
 	
-        {
-                Timer timer("Time to search all binary values: ");
+	{
+		Timer timer("Time to binary search all values: ");
 
-                int found = 0;
-                for (size_t i = 0; i < search.size(); i++)
-                {
-                        if (binarySearch(numbers, search[i]))
-                                found++;
-                }
+		int found = 0;
+		for (size_t i = 0; i < search.size(); i++)
+		{
+			if (binarySearch(numbers, search[i]))
+				found++;
+		}
 
-                std::cout << "Found "<< found << "/"
-                        << search.size() << " values." << std::endl;
-        }
+		std::cout << "Found "<< found << "/"
+			<< search.size() << " values." << std::endl;
+	}
 
-        {
-                Timer timer("Time to search all binary values with pointer: ");
+	{
+		Timer timer("Time to binary search all values (pointers): ");
 
-                int found = 0;
-                for (size_t i = 0; i < search.size(); i++)
-                {
-                        if (binarySearch(numbers.data(), numbers.data() + numbers.size(),
-                                        search[i]))
-                                found++;
-                }
+		int found = 0;
+		for (size_t i = 0; i < search.size(); i++)
+		{
+			if (binarySearch(numbers.data(), numbers.data() + numbers.size(),
+					search[i]))
+				found++;
+		}
 
-                std::cout << "Found "<< found << "/"
-                        << search.size() << " values." << std::endl;
+		std::cout << "Found "<< found << "/"
+			<< search.size() << " values." << std::endl;
+	}
+	
+
+	{
+		Timer timer("Time to binary search all values while using recursion: ");
+		int found = 0;
+		for(size_t i = 0; i < search.size(); i++)
+		{
+
+		binarySearchRecursive(numbers.data(), numbers.data() + numbers.size(),search[i]);
+		found++;
+		}
+		std::cout << "Found "<< found << "/"
+			<< search.size() << " values." << std::endl;
+	}
+	
 		
+	bubblesort (search.data(), search.data() + search.size());
+	for (unsigned int i = 0; i < search.size(); i++)
+	{
+		std::cout << search[i] << std::endl;
+	
+	}
+
 	return 0;
+
 }
-
-
-
