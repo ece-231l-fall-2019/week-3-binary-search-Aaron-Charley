e7f1879cb87675f3d1d6f06d357ae06d818184f2
Aaron-Charley <aacharley@unm.edu>
2019-09-27 14:49:01 -0600

    Update search.h

--- search.h
+++ search.h
@@ -4,9 +4,12 @@
 // Binary search functions
 bool binarySearch(const std::vector<int>& set, int value);
 bool binarySearch(const int *begin, const int *end, int value);
+bool binarySearchRecursive( const int *begin, const int *end, int value);
 
 // Linear search functions
 bool linearSearch(const std::vector<int>& set, int value);
 bool linearSearch(const int *begin, const int *end, int value);
+void bubblesort(int *begin, const int *end);
+
 
 #endif // __ECE231_SEARCH_H__
